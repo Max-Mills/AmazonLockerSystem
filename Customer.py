eea6b7272ff9fbb01051c9b151739a78bef1d2b9
Samara Ruiz Sandoval <rsandoval.samara@gmail.com>
2022-06-21 08:43:28 -0400

    Added Amazon Class and Main class

--- Customer.py
+++ Customer.py
@@ -4,11 +4,11 @@ from sympy import Order
 class Customer:
     
     # Init method that will construct objects (also known as the construtor method)
-    def __init__(self, name, geoLocation, orders, money):
-        self.customerID = None      #instance variable
+    def __init__(self, id, name, geoLocation, money):
+        self.customerID = id      #instance variable
         self.__name = name
         self.__geoLocation = geoLocation 
-        self.__orders = None
+        self.__orders = {}
         self.__money = money
 
     def setName(self, name):
@@ -23,13 +23,12 @@ class Customer:
     def getGeoLocation(self):
         return self.__geoLocation
 	
-    def setOrders(self, newOrder):
-        self.__orders.append(newOrder) 
+    #def addOrders(self, newOrder):
+        #self.__orders.append(newOrder) 
     
-    def getOrders(self):
-        return self.__orders
+    #def getAllOrders(self):
+        #return self.__orders
 
-        
     def setMoney(self, money):
         self.__money = money 
     
