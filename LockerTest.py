6c3b46c6c2c540cf1a2000005476dbca789b9ae9
Max <max33134@gmail.com>
2022-06-20 21:09:13 -0400

    added Sizes to Item and Locker
    With the Sizes enum created, I've replaced the str with the Sizes reference with Item, Locker, and LockerTest

--- LockerTest.py
+++ LockerTest.py
@@ -4,13 +4,14 @@ from Locker import Locker
 from Store import Store
 from Item import Item
 from LockerStatus import LockerStatus
+from Sizes import Sizes
 
-
-item = Item("a","toy",20.00, "medium")
+size = Sizes
+item = Item("a","toy",20.00, size.m)
 lockerStatus = LockerStatus
-locker1 = Locker("1", "small",lockerStatus.Available, "1112")
-locker2 = Locker("2", "medium",lockerStatus.Open, "1112")
-locker3 = Locker("3", "medium",lockerStatus.Available, "1112")
+locker1 = Locker("1", size.s,lockerStatus.Available, "1112")
+locker2 = Locker("2", size.m,lockerStatus.Open, "1112")
+locker3 = Locker("3", size.m,lockerStatus.Available, "1112")
 lockers = []
 
 lockers.append(locker1)
