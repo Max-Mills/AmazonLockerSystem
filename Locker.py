d9a9fa8b51801047a4444fd54c4eb7843ecc1cda
Max <max33134@gmail.com>
2022-06-26 14:42:59 -0400

    Created Warehouse; Fixed Locker and Store
    Created
    -the Warehouse class that will ship items to store
    
    Changed
    -Added the ability to add/remove items to lockers
    -Added a find locker function to seperate it from remove locker

--- Locker.py
+++ Locker.py
@@ -1,21 +1,40 @@
 from LockerStatus import LockerStatus
 from Sizes import Sizes
+from Item import Item
 
 class Locker:
 
-	def __init__(self, id: str, size: Sizes, status: LockerStatus, code: str):
+	def __init__(self, id: str, size: Sizes, status: LockerStatus, code: str, item: Item = None):
 		self.__id = id
 		self.__size = size
 		self.__status = status
 		self.__code = code
+		self.__item = item
 
 	##Locker ID and Size will never change##
+
+	def addItem(self, item: Item) -> bool:
+		'''Returns true if added, returns false if locker was already in use'''
+		if self.__item == None:
+			self.__item = item
+			return True
+		return False
+
+	def removeItem(self) -> bool:
+		'''Returns true if remove, false if locker was already empty'''
+		if self.__item != None:
+			self.__item = None
+			return True
+		return False
 
 	def changeLockerStatus(self, status: LockerStatus):
 		self.__status = status
 
 	def changeLockerCode(self, code: str):
 		self.__code = code
+
+	def getItem(self) -> Item:
+		return self.__item
 
 	def getLockerID(self) -> str:
 		return self.__id
--- LockerTest.py
+++ LockerTest.py
@@ -7,8 +7,7 @@ from Sizes import Sizes
 from GeoLocation import GeoLocation
 from AmazonLocation import AmazonLocation
 from LocationTiming import LocationTiming
-
-
+from Warehouse import Warehouse
 
 size = Sizes
 item = Item("a","toy",20.00, size.m)
@@ -16,6 +15,8 @@ lockerStatus = LockerStatus
 geoLocation = GeoLocation(2,4)
 locationTiming = LocationTiming([[7,12]], [[7,18]], [[6,12]], [[5,7]], [], [[15,18]], [], ["2022-06-26"])
 amazonLocation = AmazonLocation("asildnn", geoLocation, locationTiming)
+warehouse = Warehouse(amazonLocation)
+warehouse.addItem(item.getItemId())
 
 locker1 = Locker("1", size.s,lockerStatus.Available, "1112")
 locker2 = Locker("2", size.m,lockerStatus.Open, "1112")
@@ -25,14 +26,14 @@ lockers = []
 lockers.append(locker1)
 lockers.append(locker2)
 
-
 store = Store(amazonLocation, lockers)
 store.addLocker(locker3)
 store.findOpenLocker(item.getItemSize())
 store.removeLocker("3")
 store.findOpenLocker(item.getItemSize())
 locker2.changeLockerStatus(lockerStatus.Available)
-store.findOpenLocker(item.getItemSize())
-
-print(store.isOpen())
+locker = store.findOpenLocker(item.getItemSize())
+warehouse.shipItem(item.getItemId())
+locker.addItem(item)
+warehouse.shipItem(item.getItemId())
 
